833ed4a3239259129395061d9d864cee62b9ab2d
S8leeJ <jenna.snow.lee@gmail.com>
2023-09-04 20:24:44 -0500

    mongo

--- app.js
+++ app.js
@@ -1,6 +1,36 @@
-const express = reqire("express")
-const app = express()
+const express = require("express");
+const app = express();
+const mongoose=require("mongoose");
+app.use(express.json());
+
+const mongoUrl = 
+"mongodb+srv://jennasnowlee:<password>@cluster0.t9jljsn.mongodb.net/?retryWrites=true&w=majority";
+
+mongoose
+.connect(mongoUrl,{
+    useNewUrlParser: true,
+})
+.then(() => {
+    console.log("Connected to database");
+})
+.catch((e) => console.log(e));
 
 app.listen(5000,()=>{
-    console.log("server started")
+    console.log("server started");
 })
+
+app.post("/post", async(req,res)=>{
+    console.log(req.body);
+    const{ data } = req.body;
+    
+    try{
+        if(data == "x"){
+            res.send({status: "ok"});
+        }
+        else{
+            res.send({status: "User Not found" });
+        }
+    } catch(error){
+        res.send({status: "Something went wrong try again" });
+    }
+});
